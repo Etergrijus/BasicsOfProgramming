5021574e9ea420bbb87da1384ea0f9af8885d89f
Grimthorn <pavel.kutcin@mail.ru>
2023-04-09 13:17:54 +0300

    commit for save

--- libs/data_structures/vector/vector.h
+++ libs/data_structures/vector/vector.h
@@ -12,6 +12,8 @@ exit(1); \
 
 #define filename "inf.txt"
 
+#define separator printf("---------------\n")
+
 typedef struct Vector {
     int *data;
     size_t size;
@@ -35,6 +37,10 @@ void deleteVector(Vector *v);
 bool isEmpty(Vector v);
 
 bool isFull(Vector v);
+
+int getVectorValue(Vector v, size_t i);
+
+void pushBack(Vector *v);
 
 void outputVector(Vector v);
 
--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 #include "libs/data_structures/vector/vector.h"
 
 void test_of_basic_functional_1() {
-    printf("first_test_of_basic_memory_functional:\n");
+    printf("first_test_of_basic_functional:\n");
     Vector v = createVector(7);
     reserve(&v, 0);
     fillPartOfVector(&v, 10);
@@ -16,11 +16,11 @@ void test_of_basic_functional_1() {
     printf("anotherCheckOfFullness - %d\n", isFull(v));
     outputVector(v);
     printf("Capacity of v - %zu\n", v.capacity);
-    printf("---------------\n");
+    separator;
 }
 
 void test_of_basic_functional_2() {
-    printf("second_test_of_basic_memory_functional:\n");
+    printf("second_test_of_basic_functional:\n");
     Vector v = createVector(0);
     fillPartOfVector(&v, 6);
 
@@ -31,11 +31,11 @@ void test_of_basic_functional_2() {
     printf("checkOfFullness - %d\n", isFull(v));
     outputVector(v);
     printf("Capacity of v - %zu\n", v.capacity);
-    printf("---------------\n");
+    separator;
 }
 
 void test_of_basic_functional_3() {
-    printf("third_test_of_basic_memory_functional:\n");
+    printf("third_test_of_basic_functional:\n");
     Vector v = createVector(3);
     fillPartOfVector(&v, 1);
     fillPartOfVector(&v, 3);
@@ -47,11 +47,11 @@ void test_of_basic_functional_3() {
     clear(&v);
     fillPartOfVector(&v, 4);
     outputVector(v);
-    printf("---------------\n");
+    separator;
 }
 
 void test_of_basic_functional_4() {
-    printf("fourth_test_of_basic_memory_functional:\n");
+    printf("fourth_test_of_basic_functional:\n");
     Vector v = createVector(7);
     fillPartOfVector(&v, 6);
     reserve(&v, 2);
@@ -60,7 +60,43 @@ void test_of_basic_functional_4() {
     deleteVector(&v);
     printf("checkOfEmptiness - %d\n", isEmpty(v));
     outputVector(v);
-    printf("---------------\n");
+    separator;
+}
+
+void test_of_basic_functional_5() {
+    printf("fifth_test_of_basic_functional:\n");
+    Vector v = createVector(3);
+    fillPartOfVector(&v, 2);
+    reserve(&v, 12);
+    fillPartOfVector(&v, 9);
+
+    int i = 4;
+    printf("%d-th element of vector - %d\n", i, getVectorValue(v, i));
+    i = 12;
+    printf("%d-th element of vector - %d\n", i, getVectorValue(v, i));
+
+    deleteVector(&v);
+    v = createVector(2);
+    fillPartOfVector(&v, 2);
+    outputVector(v);
+    separator;
+}
+
+void test_pushBack_emptyVector() {
+    Vector v = createVector(0);
+    for (int i = 0; i < 10; i++)
+        pushBack(&v);
+    outputVector(v);
+    separator;
+}
+
+void test_pushBack_fullVector() {
+    Vector v = createVector(10);
+    fillPartOfVector(&v, 10);
+
+    for (int i = 0; i < 4; i++)
+        pushBack(&v);
+    outputVector(v);
 }
 
 void tests() {
@@ -71,6 +107,10 @@ void tests() {
     test_of_basic_functional_2();
     test_of_basic_functional_3();
     test_of_basic_functional_4();
+    test_of_basic_functional_5();
+
+    test_pushBack_emptyVector();
+    test_pushBack_fullVector();
 }
 
 int main() {
